f7a0f741d1ef870cd95876a1d679c2175ec06456
Finn Tegeler <developing@finn-tegeler.de>
2022-02-09 15:31:30 +0100

    Added the cvc round

---
+++ src/main/java/de/throwstnt/developing/labymod/cvc/api/game/CvcRound.java
@@ -0,0 +1,10 @@
+package de.throwstnt.developing.labymod.cvc.api.game;
+
+public class CvcRound {
+
+    private long startedAt;
+
+    public CvcRound() {
+        this.startedAt = System.currentTimeMillis();
+    }
+}
